04efcb9e62f437a2a6f213deae664c91ac1c5c7d
Armin <58628813+Armin0x@users.noreply.github.com>
2020-04-01 17:50:40 +0200

    Add files via upload

---
+++ Class 04 Task One/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Class_4_Homework_1
+{
+    class Program
+    {
+        static void Substrings(string message)
+        {
+
+            int num = NumberChar(message.Length);
+            string substringMessage = message.Substring(0, num);
+            int newLength = substringMessage.Length;
+            Console.WriteLine($"The new string is  {substringMessage.ToUpper()} and it has {newLength} characters");
+        }
+        static int NumberChar(int messageChar)
+        {
+            string inputNumber;
+            int number;
+            bool numberCheck;
+            while (true)
+            {
+                Console.WriteLine("Enter the number of character.");
+                inputNumber = Console.ReadLine();
+                numberCheck = int.TryParse(inputNumber, out number);
+                if (numberCheck)
+                {
+                    if (number > 1 && number < messageChar)
+                    {
+                        return number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Enter number between 1 and {messageChar}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Please input a valid number between 1 and {messageChar}");
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string message = "Hello from SEDC Codecademy v7.0";
+            Substrings(message);
+            Console.ReadLine();
+        }
+    }
+}
